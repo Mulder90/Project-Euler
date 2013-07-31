5f8120b98648c2fc429243a0de4361f55541ab12
Mulder90 <lore.cinque@gmail.com>
2013-07-31 22:42:55 +0200

    problem 19
    very simple with python calendar!

--- Problem19.py
+++ Problem19.py
@@ -2,14 +2,21 @@
 
 
 import time
+import calendar
 
 
 def solve():
-	pass
+	count = 0
+	for year in xrange(1901, 2001):
+		for month in xrange(1, 13):
+			day = calendar.weekday(year, month, 1)
+			if day == 6:
+				count += 1
+	return count
 
 
 if __name__ == '__main__':
-	startTime = time.time()
-	solve()
-	elapsedTime = time.time() - startTime
-	print "Time elapsed: {0}".format(elapsedTime)
+	start_time = time.time()
+	print "Result: {0}".format(solve())
+	elapsed_time = time.time() - start_time
+	print "Time elapsed: {0}".format(elapsed_time)
