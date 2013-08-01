28e0a404b89a577052250648074c5274fb5674b1
Mulder90 <lore.cinque@gmail.com>
2013-08-01 16:33:55 +0200

    pep8 indentation

--- Problem19.py
+++ Problem19.py
@@ -8,15 +8,15 @@ import calendar
 def solve():
     count = 0
     for year in xrange(1901, 2001):
-    	for month in xrange(1, 13):
-    		first_day = calendar.weekday(year, month, 1)
-    		if first_day == 6:
-    			count += 1
+        for month in xrange(1, 13):
+            first_day = calendar.weekday(year, month, 1)
+            if first_day == 6:
+                count += 1
     return count
 
 
 if __name__ == '__main__':
-	start_time = time.time()
-	print "Result: {0}".format(solve())
-	elapsed_time = time.time() - start_time
-	print "Time elapsed: {0}".format(elapsed_time)
+    start_time = time.time()
+    print "Result: {0}".format(solve())
+    elapsed_time = time.time() - start_time
+    print "Time elapsed: {0}".format(elapsed_time)
