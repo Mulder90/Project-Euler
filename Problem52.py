e22ca882535645ea37e590db02a522b801298680
Lorenzo Cinque <lore.cinque@gmail.com>
2013-08-29 10:26:41 +0200

    Update Problem 52

--- Problem52.py
+++ Problem52.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 
+import itertools
 import time
 import math
 
@@ -10,20 +11,18 @@ def digits(n):
 
 
 def solve():
-    i = 1
-    while True:
-        if int(math.log10(i)) + 1 == int(math.log10(i * 6)) + 1:
+    for i in itertools.count(1):
+        len_number = int(math.log10(i)) + 1
+        if len_number == int(math.log10(i * 6)) + 1:
             values = [digits(i * j) for j in range(2, 7)]
             if all([digits(i) == value for value in values]):
                 return i
-            else:
-                i += 1
         else:
-            i = int(str('1' + ('0' * (int(math.log10(i)) + 1))))
+            i = int(str('1' + ('0' * len_number)))
 
 
 if __name__ == '__main__':
     start_time = time.time()
-    print("Result: {0}".format(solve()))
+    print("Result: {result}".format(result=solve()))
     elapsed_time = time.time() - start_time
-    print("Elapsed time: {0}".format(elapsed_time))
+    print("Elapsed time: {time}".format(time=elapsed_time))
